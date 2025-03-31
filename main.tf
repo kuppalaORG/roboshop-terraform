63af07a2e7bbc971411b22e70f41148fbf7eea4f
bharathkuppala <bharath.kuppala@simplifyvms.com>
2025-03-31 21:50:22 +0530

    subnets and azs

--- main.tf
+++ main.tf
@@ -2,4 +2,9 @@ module "vpc" {
   source = "./modules/vpc"
   cidr = var.vpc["cidr"]
   env  = var.env
+  public_subnets = var.vpc["public_subnets"]
+  web_subnets = var.vpc["web_subnets"]
+  app_subnets = var.vpc["app_subnets"]
+  db_subnets = var.vpc["db_subnets"]
+  availabilty_zones = var.vpc["availabilty_zones"]
 }
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -6,3 +6,45 @@ resource "aws_vpc" "main" {
     Name =  "${var.env}-vpc"
   }
 }
+
+#subnets
+resource "aws_subnet" "public"{
+  count      = length(var.public_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.public_subnets[count.index]
+  availability_zone = var.availabilty_zones[count.index]
+  tags = {
+    Name = "public-subnet-${split("-", var.availabilty_zones[count.index])[2]}"
+  }
+}
+
+resource "aws_subnet" "web"{
+  count      = length(var.web_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.web_subnets[count.index]
+  availability_zone = var.availabilty_zones[count.index]
+  tags = {
+    Name = "web-subnet-${split("-", var.availabilty_zones[count.index])[2]}"
+  }
+}
+
+resource "aws_subnet" "app"{
+  count      = length(var.app_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.app_subnets[count.index]
+  availability_zone = var.availabilty_zones[count.index]
+  tags = {
+    Name = "app-subnet-${split("-", var.availabilty_zones[count.index])[2]}"
+  }
+}
+
+resource "aws_subnet" "db"{
+  count      = length(var.db_subnets)
+  vpc_id     = aws_vpc.main.id
+  cidr_block = var.db_subnets[count.index]
+  availability_zone = var.availabilty_zones[count.index]
+  tags = {
+    Name = "db-subnet-${split("-", var.availabilty_zones[count.index])[2]}"
+  }
+}
+
