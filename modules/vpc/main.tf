040253a14388e4fb6b51741e8266d4b9eb90564e
BHarath <bharathkumarkuppala@gmail.com>
2025-03-31 23:13:49 +0530

    added app,web,db route

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -7,17 +7,6 @@ resource "aws_vpc" "main" {
   }
 }
 
-#route tables
-resource "aws_route_table" "public" {
-  count =  length(var.public_subnets)
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
-  }
-}
-
-
 #subnets
 resource "aws_subnet" "public"{
   count      = length(var.public_subnets)
@@ -58,4 +47,44 @@ resource "aws_subnet" "db"{
     Name = "db-subnet-${split("-", var.availability_zones[count.index])[2]}"
   }
 }
+
+
+
+#route tables
+resource "aws_route_table" "public" {
+  count =  length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+
+resource "aws_route_table" "web" {
+  count =  length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "web-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+
+resource "aws_route_table" "app" {
+  count =  length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "app-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+
+resource "aws_route_table" "db" {
+  count =  length(var.public_subnets)
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "db-rt-${split("-", var.availability_zones[count.index])[2]}"
+  }
+}
+
 
