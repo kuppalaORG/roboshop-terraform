6b249e5ec1906f5b78d28d5240526ed0ff751937
BHarath <bharathkumarkuppala@gmail.com>
2025-03-31 23:44:50 +0530

    added internet gateway to public rt

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -55,6 +55,11 @@ resource "aws_route_table" "public" {
   count =  length(var.public_subnets)
   vpc_id = aws_vpc.main.id
 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
   tags = {
     Name = "public-rt-${split("-", var.availability_zones[count.index])[2]}"
   }
