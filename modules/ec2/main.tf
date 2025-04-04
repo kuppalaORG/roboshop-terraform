2d9ee5140303bb118ba459af1018d5750590071a
BHarath <bharathkumarkuppala@gmail.com>
2025-04-04 23:20:51 +0530

    auto scaling group

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -1,4 +1,4 @@
-resource "aws_security_group" "allow_tls" {
+resource "aws_security_group" "main" {
   name        = "${var.name}-${var.env}-sg"
   description = "${var.name}-${var.env}-sg"
   vpc_id      = var.vpc_id
@@ -34,7 +34,7 @@ resource "aws_launch_template" "main" {
   name = "${var.name}-${var.env}-lt"
   image_id = data.aws_ami.rhel9.image_id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  vpc_security_group_ids = [aws_security_group.main.id]
 
   instance_market_options {
     market_type = "spot"
