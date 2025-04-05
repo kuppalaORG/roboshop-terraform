adf730a6bd591636f1f4f067de5bbd15deeffdc7
BHarath <bharathkumarkuppala@gmail.com>
2025-04-05 18:31:21 +0530

    target group attachment

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -156,23 +156,23 @@ resource "aws_lb" "main" {
   }
 }
 
-resource "aws_lb_target_group" "main" {
-  count              = var.asg ? 1 : 0
-  name               = "${var.name}"
-  port               = var.allow_port
-  protocol           = "HTTP"
-  vpc_id             = var.vpc_id
-}
-
-
-resource "aws_lb_listener" "front_end" {
-  count              = var.asg ? 1 : 0
-  load_balancer_arn = aws_lb.main.*.arn[count.index]
-  port              = "80"
-  protocol          = "HTTP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.main.*.arn[count.index]
-  }
-}
+# resource "aws_lb_target_group" "main" {
+#   count              = var.asg ? 1 : 0
+#   name               = "${var.name}"
+#   port               = var.allow_port
+#   protocol           = "HTTP"
+#   vpc_id             = var.vpc_id
+# }
+#
+#
+# resource "aws_lb_listener" "front_end" {
+#   count              = var.asg ? 1 : 0
+#   load_balancer_arn = aws_lb.main.*.arn[count.index]
+#   port              = "80"
+#   protocol          = "HTTP"
+#
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.main.*.arn[count.index]
+#   }
+# }
