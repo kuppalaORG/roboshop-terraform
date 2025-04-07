e14014869c573cf9e6f9a7c61c027094875a7078
BHarath <bharathkumarkuppala@gmail.com>
2025-04-08 00:32:32 +0530

    http redirect

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -156,7 +156,22 @@ resource "aws_lb_listener" "public-https" {
     target_group_arn = aws_lb_target_group.main.arn
   }
 }
+resource "aws_lb_listener" "public-http" {
+  count             = var.internal ? 0 :1
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
 
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+}
 
 resource "aws_route53_record" "lb" {
   zone_id = var.zone_id
