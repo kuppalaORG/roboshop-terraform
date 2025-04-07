219a7eab2c98ab27055b058ce3c1af70ff5f22bf
BHarath <bharathkumarkuppala@gmail.com>
2025-04-08 00:12:44 +0530

    decoupling and Https

--- modules/asg/main.tf
+++ modules/asg/main.tf
@@ -141,7 +141,7 @@ resource "aws_lb_listener" "public-https" {
   count             = var.internal ? 0 :1
   load_balancer_arn = aws_lb.main.arn
   port              = "443"
-  protocol          = "HTTP"
+  protocol          = "HTTPS"
   ssl_policy        = "ELBSecurityPolicy-2016-08"
   certificate_arn   = var.acm_https_arn
   default_action {
